518e2d9f2a57bf0001f4b59bd7f0bc0915ac2f6b
starblasters8 <starblasters8@gmail.com>
2023-05-23 14:45:45 -0400

    first commit

---
+++ Showcase.java
@@ -0,0 +1,37 @@
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Showcase extends JPanel
+{
+    private int maxFPS = 30;
+
+    public Showcase(int w, int h)
+    {
+        this.setPreferredSize(new Dimension(w,h));
+        setBackground(Color.BLACK);
+
+        Timer timer = new Timer(1000/maxFPS, new ActionListener() {public void actionPerformed(ActionEvent e) 
+            {   repaint();  }});    timer.start();
+    }
+
+    public void paintComponent(Graphics g)
+    {
+        super.paintComponent(g);
+        Graphics2D g2d = (Graphics2D)g;
+
+        g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); // Anti-aliasing
+        
+
+    }
+
+    // JFrame
+    public static void main(String[] args)
+    {
+        JFrame frame = new JFrame("Particle Physics");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getContentPane().add(new Showcase(800,800));
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
